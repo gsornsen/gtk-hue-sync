b24e5ba885a918b762b6e55235fc20772f9d81e9
gsornsen <gsornsen@gmail.com>
2019-09-21 15:50:36 -0700

    Added Sync class

--- src/gtk-hue-sync.py
+++ src/gtk-hue-sync.py
@@ -8,82 +8,70 @@ from rgbxy import Converter
 from rgbxy import GamutC
 from util.colors import DominantColor
 from util.config import Config
-
-config = Config()
-config_obj = config.get_config()
-ip = config_obj['bridge']['ip']
-user = config_obj['bridge']['user']
-lights_list = config_obj['lights']
-
-b = Bridge(ip, user)
-lights = b.lights
+from util.cli import CLI
 
 
-def get_cli_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-v',
-                        '--verbose',
-                        action='store_true',
-                        help='Be verbose')
-    parser.add_argument('-f',
-                        '--fullscreen',
-                        action='store_true',
-                        help='Get dominant color of full screen',
-                        default=True)
-    parser.add_argument('-i',
-                        '--interval',
-                        help='Interval to change lights in ms',
-                        default=100,
-                        type=int)
-    parser.add_argument('-t',
-                        '--transition',
-                        help='Light transition time in ms',
-                        default=100,
-                        type=int)
-    parser.add_argument('-w',
-                        '--ignore_white',
-                        action='store_true',
-                        help='Ignore white pixels',
-                        default=False)
-    parser.add_argument('-b',
-                        '--ignore_black',
-                        action='store_true',
-                        help='Ignore black pixels',
-                        default=False)
-    args = parser.parse_args()
-    return args
+class Sync:
+    def __init__(self, verbose, fullscreen, transition, interval, ignore_white,
+                 ignore_black):
+        self.config = Config()
+        # Get config from config.yaml
+        self.config_obj = self.config.get_config()
+        self.ip = self.config_obj['bridge']['ip']
+        self.user = self.config_obj['bridge']['user']
+        self.lights_list = self.config_obj['lights']
+        # Use CLI args
+        self.verbose = verbose
+        self.fullscreen = fullscreen
+        self.transition = transition
+        self.interval = interval
+        self.ignore_white = ignore_white
+        self.ignore_black = ignore_white
+        # Instantiate qHue Bridge
+        self.bridge = Bridge(self.ip, self.user)
+
+    def sync(self):
+        try:
+            # Instantiate DominantColor
+            dc = DominantColor(self.fullscreen, self.ignore_white,
+                               self.ignore_black)
+            # Get the dominant color on the primary display
+            rgb_color = dc.get_dominant_color()
+            # Instantiate the rgb to xy color Converter class
+            color_converter = Converter(GamutC)
+            # Convert the dominant color from rgb to xy
+            xy_color = color_converter.rgb_to_xy(rgb_color[0], rgb_color[1],
+                                                 rgb_color[2])
+            # Be verbose about what we're doing
+            if self.verbose is True:
+                print(f'RGB{rgb_color} ==> XY{xy_color}')
+                print(
+                    f'Fullscreen: {self.fullscreen}\nInterval: {self.interval}\nTransition: {self.transition}\n'
+                    f'Ignore White Pixels: {self.ignore_white}\nIgnore Black Pixels: {self.ignore_black}\n'
+                )
+            # Change the color of the light(s)
+            for light in self.lights_list:
+                self.bridge.lights[light].state(xy=[xy_color[0], xy_color[1]],
+                                                transitiontime=int(
+                                                    self.transition * 0.001))
+
+        except TypeError:
+            # Not enough non-black or non-white pixels to update
+            pass
 
 
-def change_colors(verbose, fullscreen, transition, interval, ignore_white,
-                  ignore_black):
-    dc = DominantColor(fullscreen, ignore_white, ignore_black)
-    rgb_color = dc.get_dominant_color()
-    color_converter = Converter(GamutC)
+def main():
     try:
-        xy_color = color_converter.rgb_to_xy(rgb_color[0], rgb_color[1],
-                                             rgb_color[2])
-
-        if verbose is True:
-            print(f'{rgb_color} ==> {xy_color} Fullscreen: {fullscreen}')
-            print(
-                f'\tInterval: {interval} Transition: {transition} Ignore W: {ignore_white} Ignore B: {ignore_black}'
-            )
-
-        for light in lights_list:
-            b.lights[light].state(xy=[xy_color[0], xy_color[1]],
-                                  transitiontime=int(transition * 0.001))
-
-    except TypeError:
-        # Not enough non-black or non-white pixels to update
-        pass
-
-
-if __name__ == '__main__':
-    try:
+        args = CLI().get_cli_args()
+        sync = Sync(args.verbose, args.fullscreen, args.transition,
+                    args.interval, args.ignore_white, args.ignore_black)
+        # Run until KeyboardInterrupt
         while True:
-            args = get_cli_args()
-            change_colors(args.verbose, args.fullscreen, args.transition,
-                          args.interval, args.ignore_white, args.ignore_black)
+            sync.sync()
             time.sleep(float(args.interval * 0.001))
     except KeyboardInterrupt:
         sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
